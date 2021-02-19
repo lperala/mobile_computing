43c9f032d17a9d76f737b080ecbc5679f80dc77e
lassiperala <lassi0205@hotmail.fi>
2021-02-19 12:42:51 +0200

    19.2.2021 listview working, no delete

--- app/src/main/java/com/example/homework1/MenuActivity.kt
+++ app/src/main/java/com/example/homework1/MenuActivity.kt
@@ -1,11 +1,18 @@
 package com.example.homework1
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import com.example.homework1.databinding.ActivityMenuBinding
+import kotlinx.android.synthetic.main.list_item.view.*
+
+
 
 class MenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMenuBinding
@@ -15,26 +22,76 @@ class MenuActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val context = this
+        val db = DataBaseHandler(context)
+        //val data = db.readDataMessages()
+
+        // ADAPTER SETIT
+
+        val messages = db.getDataMessages()
+        val arrayAdapter = MessageArrayAdapter(this, messages)
+
+        Toast.makeText(this@MenuActivity, "$messages", Toast.LENGTH_SHORT).show()
+        binding.listView.adapter = arrayAdapter
+
         // Shows the user's profile
-        binding.btnProfile.setOnClickListener{
+        binding.btnProfile.setOnClickListener {
             Log.d("Lab", "ProfileActivity Button CLicked")
             startActivity(
-                Intent(applicationContext, ProfileActivity::class.java)
+                    Intent(applicationContext, ProfileActivity::class.java)
             )
         }
 
-        binding.btnLogOut.setOnClickListener{
+        binding.btnLogOut.setOnClickListener {
             Log.d("Lab", "LogOut Button CLicked")
             startActivity(
-                Intent(applicationContext, MainActivity::class.java)
+                    Intent(applicationContext, MainActivity::class.java)
+            )
+            loggedAs = ""
+        }
+
+        binding.btnMenuHelp.setOnClickListener {
+            Log.d("Lab", "LogOut Button CLicked")
+            startActivity(
+                    Intent(applicationContext, MenuHelpActivity::class.java)
             )
         }
 
-        binding.btnMenuHelp.setOnClickListener{
-            Log.d("Lab", "LogOut Button CLicked")
+        binding.btnNewMessage.setOnClickListener {
             startActivity(
-                Intent(applicationContext, MenuHelpActivity::class.java)
+                    Intent(applicationContext, NewMessageActivity::class.java)
             )
         }
+
+        binding.listView.setOnItemClickListener(object: AdapterView.OnItemClickListener{
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(context, "LIST VIEW CLICKED AT: " + position, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
+
+
+class ListMessages(
+        val imageResource: Int,
+        val message: String,
+        val date: String
+)
+
+class MessageArrayAdapter(context: Context, messages: List<ListMessages>)
+    : ArrayAdapter<ListMessages>(context, 0, messages) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup)
+        : View {
+        val rootView = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        val currentMessage = getItem(position)
+
+        if (currentMessage != null) {
+            //rootView.messageImage.setImageResource(currentMessage.imageResource)
+            rootView.messageContent.text = currentMessage.message
+        }
+
+
+        return rootView
     }
 }
